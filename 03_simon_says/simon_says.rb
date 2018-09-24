e253d5355c3c2fdda30a5b568ca688e4f6aefd61
James Lafferty <james@j-lafferty.net>
2018-09-23 17:00:03 -0700

    Add solution to simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
 #write your code here
+def echo(word)
+    return word
+end
+
+def shout(word)
+    return word.upcase
+end
+
+def repeat(word, number = 2)
+    return ("#{word} " * number).chomp(' ')
+end
+
+def start_of_word(word, range = 1)
+    return word[0...range]
+end
+
+def first_word(word)
+    words = word.split
+    return words[0]
+end
+
+def titleize(word)
+    stop_words = %w{a an and the or for of nor over}
+    return word.split.each_with_index.map{|word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize} * " "
+end
