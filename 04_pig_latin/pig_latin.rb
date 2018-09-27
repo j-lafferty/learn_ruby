abd00b677c5f7c06df921cfe28654804a6f34268
James Lafferty <james@j-lafferty.net>
2018-09-26 21:00:39 -0700

    Add function to support multiple words and function to convert words beginning with a vowel

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+def translate(string)
+    string_to_translate = string.split
+    pig_latin_string = ""
+
+    for i in (0..(string_to_translate.length - 1))
+        pig_latin_string << convert(string_to_translate[i])
+        pig_latin_string << " "
+    end
+
+    return pig_latin_string.chomp(" ")
+end
+
+def convert(word)
+    converted_word = word.chars
+    vowels = %w[a e i o u]
+    consonants = %w[q w r t y p s d f g h j k l z x c v b n m]
+
+    for i in (0..(word.length - 1))
+		if converted_word.first == vowels[i]
+			converted_word << 'a'
+            converted_word << 'y'
+        end
+    end
+
+    return converted_word.join
+end
+
+    
