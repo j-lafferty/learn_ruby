6f7ec57e033978b50ec6cb8ddaa2b0a6f96c2d44
James Lafferty <james@j-lafferty.net>
2018-09-29 13:05:22 -0700

    Replaced 'for' loop with series of 'if/elsif' loops

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -12,18 +12,20 @@ def translate(string)
 end
 
 def convert(word)
-    converted_word = word.chars
     vowels = %w[a e i o u]
     consonants = %w[q w r t y p s d f g h j k l z x c v b n m]
 
-    for i in (0..(word.length - 1))
-		if converted_word.first == vowels[i]
-			converted_word << 'a'
-            converted_word << 'y'
-        end
+    if vowels.include?(word[0])
+        word + 'ay'
+    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        word[3..-1] + word[0..2] + 'ay'
+    elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(word[0])
+        word[1..-1] + word[0] + 'ay'
+    else
+        word
     end
-
-    return converted_word.join
 end
 
     
